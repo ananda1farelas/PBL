bfbdcbde1b6b1160c3c91b1f653bbba383a8a7ac
ananda1farelas <ananda1farelas@gmail.com>
2026-09-06 23:04:20 +0700

    penyesuaian config

---
+++ TM4/api-students/config/logger.go
@@ -0,0 +1,12 @@
+package config
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+)
+
+func LoggerMiddleware() fiber.Handler {
+	return logger.New(logger.Config{
+		Format: "[${time}] ${status} - ${latency} ${method} ${path}\n",
+	})
+}
